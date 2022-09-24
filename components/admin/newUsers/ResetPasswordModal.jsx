bd7f79a9c300494efcb9ab7e9ba9cbf5859a2062
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-09-24 21:35:29 +0530

    Refactor 'ResetPasswordModal' and 'EditUserModal' to use 'ModalHeader' component

--- components/admin/newUsers/ResetPasswordModal.jsx
+++ components/admin/newUsers/ResetPasswordModal.jsx
@@ -1,20 +1,7 @@
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
 import { useTheme } from '@mui/system';
-import {
-  AppBar,
-  IconButton,
-  Toolbar,
-  useMediaQuery,
-  Typography,
-  MenuItem,
-} from '@mui/material';
-import { useEffect, useState } from 'react';
-import { Close } from '@mui/icons-material';
+import { useState } from 'react';
+import ModalHeader from '@/components/common/ModalHeader';
 
 export default function ResetPasswordModal({
   open,
@@ -23,8 +10,6 @@ export default function ResetPasswordModal({
   handleSave,
 }) {
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
-  const width = useMediaQuery(theme.breakpoints.down('md')) ? '100%' : '400px';
 
   const [password, setPassword] = useState('');
   const [confPassword, setConfPassword] = useState('');
@@ -43,100 +28,68 @@ export default function ResetPasswordModal({
 
   return (
     <div>
-      <Dialog
+      <ModalHeader
+        title="Reset Password"
         open={open}
-        onClose={close}
-        fullScreen={fullScreen}
-        fullWidth={true}
-        maxWidth="sm"
+        handleClose={close}
+        save={save}
       >
-        {fullScreen && (
-          <AppBar sx={{ position: 'relative' }}>
-            <Toolbar>
-              <IconButton
-                edge="start"
-                color="inherit"
-                onClick={close}
-                aria-label="close"
-              >
-                <Close />
-              </IconButton>
-              <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-                Reset Password
-              </Typography>
-              <Button autoFocus color="inherit" onClick={save}>
-                Save
-              </Button>
-            </Toolbar>
-          </AppBar>
-        )}
-        {!fullScreen && <DialogTitle>Reset Password</DialogTitle>}
-        <DialogContent sx={{ p: 5 }}>
-          <TextField
-            margin="dense"
-            id="name"
-            label="Name"
-            value={user ? user.name : ''}
-            type="text"
-            fullWidth
-            disabled
-            variant="standard"
-          />
-          <TextField
-            margin="dense"
-            id="username"
-            label="Username"
-            value={user ? user.username : ''}
-            type="text"
-            fullWidth
-            disabled
-            variant="standard"
-            sx={{ mt: 3 }}
-          />
-          <TextField
-            margin="dense"
-            id="type"
-            label="Type"
-            value={
-              user ? (user.type === 'doctor' ? 'Doctor' : 'Consultant') : ''
-            }
-            type="text"
-            fullWidth
-            disabled
-            variant="standard"
-            sx={{ mt: 3 }}
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="new-password"
-            label="New Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            fullWidth
-            variant="standard"
-            sx={{ mt: 3 }}
-          />
-          <TextField
-            margin="dense"
-            id="conf-password"
-            label="Confirm Password"
-            value={confPassword}
-            onChange={(e) => setConfPassword(e.target.value)}
-            type="password"
-            fullWidth
-            variant="standard"
-            sx={{ mt: 3 }}
-          />
-        </DialogContent>
-        {!fullScreen && (
-          <DialogActions>
-            <Button onClick={close}>Cancel</Button>
-            <Button onClick={save}>Save</Button>
-          </DialogActions>
-        )}
-      </Dialog>
+        <TextField
+          margin="dense"
+          id="name"
+          label="Name"
+          value={user ? user.name : ''}
+          type="text"
+          fullWidth
+          disabled
+          variant="standard"
+        />
+        <TextField
+          margin="dense"
+          id="username"
+          label="Username"
+          value={user ? user.username : ''}
+          type="text"
+          fullWidth
+          disabled
+          variant="standard"
+          sx={{ mt: 3 }}
+        />
+        <TextField
+          margin="dense"
+          id="type"
+          label="Type"
+          value={user ? (user.type === 'doctor' ? 'Doctor' : 'Consultant') : ''}
+          type="text"
+          fullWidth
+          disabled
+          variant="standard"
+          sx={{ mt: 3 }}
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="new-password"
+          label="New Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          fullWidth
+          variant="standard"
+          sx={{ mt: 3 }}
+        />
+        <TextField
+          margin="dense"
+          id="conf-password"
+          label="Confirm Password"
+          value={confPassword}
+          onChange={(e) => setConfPassword(e.target.value)}
+          type="password"
+          fullWidth
+          variant="standard"
+          sx={{ mt: 3 }}
+        />
+      </ModalHeader>
     </div>
   );
 }
