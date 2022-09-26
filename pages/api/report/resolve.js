8405f1a2ba476b7c72589c59f6fb5e103e0289ef
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-09-26 21:08:34 +0530

    Add api endpoint for marking a report as resolved

---
+++ pages/api/report/resolve.js
@@ -0,0 +1,38 @@
+import { getLoginSession } from '@/lib/auth/session';
+import dbConnect from '@/lib/db';
+import Report from '@/lib/models/Report';
+
+export default async function resolve(req, res) {
+  /**
+   * TODO: Verify whether a consultant logged in
+   */
+
+  try {
+    if (req.method !== 'PUT') {
+      return res.status(405).end();
+    }
+
+    const session = await getLoginSession(req);
+
+    /** @type {import('@/lib/models/Report').ReportEntity | null} */
+    let report = null;
+
+    if (session) {
+      await dbConnect();
+
+      const { postId } = req.body;
+      report = await Report.findOneAndUpdate(
+        { _id: postId },
+        { resolved: true },
+        { new: true }
+      );
+
+      if (!report) return res.status(404).json({ message: 'Report not found' });
+
+      res.status(200).json({ report });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).end('Authentication token is invalid, please log in');
+  }
+}
