2b741a1572210666adec420152b85565fa169e83
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2022-09-27 06:32:28 +0530

    Add api route for editing a user

---
+++ pages/api/user/[_id].js
@@ -0,0 +1,38 @@
+import { getLoginSession } from '@/lib/auth/session';
+import dbConnect from '@/lib/db';
+import User from '@/lib/models/User';
+
+export default async function updateUser(req, res) {
+  /**
+   * TODO: Verify whether an admin logged in
+   * TODO: Validate the request body
+   */
+  try {
+    const session = await getLoginSession(req);
+
+    /** @type {import('@/lib/models/User').UserEntity|null} */
+    let user = null;
+
+    if (session) {
+      await dbConnect();
+
+      const { _id } = req.query;
+      const { name, type } = req.body;
+
+      user = await User.findByIdAndUpdate(
+        _id,
+        { name, type },
+        {
+          new: true,
+        }
+      );
+
+      if (!user) return res.status(404).json({ message: 'User not found' });
+
+      res.status(200).json({ user });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).end('Authentication token is invalid, please log in');
+  }
+}
