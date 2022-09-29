618c09837371ec3ed504e5383e5b8e55a775cdbc
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-09-29 18:05:45 +0530

    Add login page

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,9 +1,18 @@
 import { getLoginSession } from '@/lib/auth/session';
 import { send } from '@/lib/util';
-import Router from 'next/router';
+import {
+  Alert,
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import Router, { useRouter } from 'next/router';
 import { useRef, useState } from 'react';
 
 export default function LoginPage() {
+  const router = useRouter();
   const [errorMsg, setErrorMsg] = useState('');
 
   const usernameRef = useRef(null);
@@ -26,23 +35,76 @@ export default function LoginPage() {
     }
   }
 
+  /** @param {string} href */
+  const getRedirector = (href) => () => {
+    router.push(href);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-      {errorMsg && <div className="error">Error: {errorMsg}</div>}
-      <div>
-        <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" ref={usernameRef} />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          ref={passwordRef}
-        />
-      </div>
-      <button type="submit">Login</button>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        rowSpacing={5}
+        marginTop="5px"
+      >
+        <Grid item>
+          <Typography variant="h4">Login</Typography>
+        </Grid>
+      </Grid>
+      <Container sx={{ marginTop: '15px' }}>
+        {errorMsg && (
+          <Alert severity="error" sx={{ marginY: '10px' }}>
+            {errorMsg}
+          </Alert>
+        )}
+        <Grid
+          container
+          alignItems="center"
+          justifyContent="center"
+          rowSpacing={5}
+        >
+          <Grid item xs={3} md={5}></Grid>
+          <Grid item container xs={6} md={2} justifyContent="center">
+            <TextField
+              variant="standard"
+              label="Username"
+              inputRef={usernameRef}
+            />
+          </Grid>
+          <Grid item xs={3} md={5}></Grid>
+          <Grid item xs={3} md={5}></Grid>
+          <Grid item container xs={6} md={2} justifyContent="center">
+            <TextField
+              variant="standard"
+              label="Password"
+              inputRef={passwordRef}
+              type="password"
+            />
+          </Grid>
+          <Grid item xs={3} md={5}></Grid>
+        </Grid>
+      </Container>
+      <Grid container justifyContent="space-between" sx={{ marginTop: '55px' }}>
+        <Grid item xs={3} md={5} />
+        <Grid item container xs={4} md={1} justifyContent="center">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={getRedirector('/auth/register')}
+          >
+            Register
+          </Button>
+        </Grid>
+        <Grid item xs={0} md={0} />
+        <Grid item container xs={3} md={1} justifyContent="center">
+          <Button variant="contained" color="success" type="submit">
+            Login
+          </Button>
+        </Grid>
+        <Grid item xs md={5} />
+      </Grid>
     </form>
   );
 }
