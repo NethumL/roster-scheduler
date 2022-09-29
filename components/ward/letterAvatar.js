2e780d18fd09fa7586cb02a03cf31b09177ac750
Samindra Kumari <samindrakumarihr@gmail.com>
2022-09-29 11:02:19 +0530

    Custom avatar added

---
+++ components/ward/letterAvatar.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+
+function stringAvatar(name) {
+  if (name.split(' ').length == 1) {
+    return {
+      sx: {
+        bgcolor: '#8cbff2',
+      },
+      children: `${name.split(' ')[0][0]}`,
+    };
+  } else {
+    return {
+      sx: {
+        bgcolor: '#8cbff2',
+      },
+      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    };
+  }
+}
+
+export default function LetterAvatars(ward) {
+  return <Avatar {...stringAvatar(ward.name)} />;
+}
