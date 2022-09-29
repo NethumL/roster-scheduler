cf17aa73131e280098076749ff04f0e5fa166982
Samindra Kumari <samindrakumarihr@gmail.com>
2022-09-29 10:55:16 +0530

    Modal for ward delete confirmation added

---
+++ components/ward/doctors/deleteConfirmModal.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function deleteConfirm(open, handleClose) {
+  return (
+    <div>
+      <Dialog
+        open={open.open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'DELETE WARD'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this ward?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={open.handleClose}>CANCEL</Button>
+          <Button onClick={open.handleClose} autoFocus>
+            DELETE
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
