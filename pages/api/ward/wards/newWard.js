e04b7c132a2b966136d4d1416b67f3a4ea42b1b6
Samindra Kumari <samindrakumarihr@gmail.com>
2022-10-03 13:00:02 +0530

    create new ward i db functionality implemented

--- pages/api/ward/wards/newWard.js
+++ pages/api/ward/wards/newWard.js
@@ -1,12 +1,11 @@
-import { getLoginSession } from '@/lib/auth/session';
+// import { getLoginSession } from '@/lib/auth/session';
 import dbConnect from '@/lib/db';
 import Ward from '@/lib/models/Ward';
 
-export default async function ward(req, res) {
+export default async function newWard(req, res) {
   /**
-   * TODO: Verify whether a doctor logged in
+   * TODO: Verify whether an admin logged in
    * TODO: Validate user input
-   * TODO: Include user id
    */
 
   try {
@@ -14,29 +13,39 @@ export default async function ward(req, res) {
       return res.status(405).end();
     }
 
-    const session = await getLoginSession(req);
+    // const session = await getLoginSession(req);
 
-    /** @type {import('@/lib/models/Report').ReportEntity | null} */
+    /** @type {import('@/lib/models/Ward').WardEntity | null} */
     let ward = null;
+    // if (session) {
+    await dbConnect();
 
-    if (session) {
-      await dbConnect();
+    const {
+      name,
+      description,
+      personInCharge,
+      shifts,
+      minNumberOfDoctors,
+      maxNumberOfLeaves,
+      minNumberOfDoctorsPerShift,
+      allowAdjacentShifts,
+    } = req.body;
+    ward = new Ward({
+      name,
+      description,
+      personInCharge,
+      shifts,
+      minNumberOfDoctors,
+      maxNumberOfLeaves,
+      minNumberOfDoctorsPerShift,
+      allowAdjacentShifts,
+      doctors: [],
+    });
 
-      const { subject, description } = req.body;
-      ward = new Ward({
-        name,
-        description,
-        personInCharge,
-        numDutyCycles,
-        numDoctors,
-        maxNumLeaves,
-        minNumDoctorsPerShift,
-        statusAdjacentShifts,
-      });
-
-      await ward.save();
-      res.status(200).json({ ward });
-    }
+    await ward.save();
+    res.status(200).json({ ward });
+    console.log(ward);
+    // }
   } catch (error) {
     console.error(error);
     res.status(500).end('Authentication token is invalid, please log in');
