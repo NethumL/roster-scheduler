5190fdc166a9d210bd59663190737cbf25f7a4b1
Samindra Kumari <samindrakumarihr@gmail.com>
2022-10-03 12:58:47 +0530

    edit ward in db functionality implemented

--- pages/api/ward/wards/editWard.js
+++ pages/api/ward/wards/editWard.js
@@ -1,4 +1,4 @@
-import { getLoginSession } from '@/lib/auth/session';
+// import { getLoginSession } from '@/lib/auth/session';
 import dbConnect from '@/lib/db';
 import Ward from '@/lib/models/Ward';
 
@@ -12,41 +12,40 @@ export default async function editWard(req, res) {
       return res.status(405).end();
     }
 
-    const session = await getLoginSession(req);
+    // const session = await getLoginSession(req);
 
     /** @type {import('@/lib/models/Ward').WardEntity | null} */
-    let report = null;
+    let ward = null;
 
-    if (session) {
-      await dbConnect();
+    // if (session) {
+    await dbConnect();
 
-      const {
-        wardId,
-        name,
-        description,
-        personInCharge,
-        numDutyCycles,
-        numDoctors,
-        maxNumLeaves,
-        minNumDoctorsPerShift,
-        statusAdjacentShifts,
-      } = req.body;
-      ward = await Ward.findByIdAndUpdate(
-        wardId,
-        { name: name },
-        { description: description },
-        { personInCharge: personInCharge },
-        { numDutyCycles: numDutyCycles },
-        { numDoctors: numDoctors },
-        { maxNumLeaves: maxNumLeaves },
-        { minNumDoctorsPerShift: minNumDoctorsPerShift },
-        { statusAdjacentShifts: statusAdjacentShifts }
-      );
+    const {
+      _id,
+      name,
+      description,
+      personInCharge,
+      shifts,
+      minNumberOfDoctors,
+      maxNumberOfLeaves,
+      minNumberOfDoctorsPerShift,
+      allowAdjacentShifts,
+    } = req.body;
+    ward = await Ward.findByIdAndUpdate(_id, {
+      name: name,
+      description: description,
+      personInCharge: personInCharge,
+      shifts: shifts,
+      minNumberOfDoctors: minNumberOfDoctors,
+      maxNumberOfLeaves: maxNumberOfLeaves,
+      minNumberOfDoctorsPerShift: minNumberOfDoctorsPerShift,
+      allowAdjacentShifts: allowAdjacentShifts,
+    });
 
-      if (!ward) return res.status(404).json({ message: 'Report not found' });
+    if (!ward) return res.status(404).json({ message: 'Ward not found' });
 
-      res.status(200).json({ ward });
-    }
+    res.status(200).json({ ward });
+    // }
   } catch (error) {
     console.error(error);
     res.status(500).end('Authentication token is invalid, please log in');
