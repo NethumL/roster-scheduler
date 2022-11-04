132b17f88d4d1b600039780e80ca8a208df81a95
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-11-04 13:56:51 +0530

    Add ortools python library

--- api/roster/schedule.py
+++ api/roster/schedule.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 from http.server import BaseHTTPRequestHandler
 
+from ortools.sat.python import cp_model
+
 
 # Example serverless function
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        model = cp_model.CpModel()
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
