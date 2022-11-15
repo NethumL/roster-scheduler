2752b9b2d883ec7ceee82c10cfaf25c6f50b69cf
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-11-15 23:46:35 +0530

    Fix passing of user to view
    Now every page gets the user object if defined

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import Layout from '@/components/common/Layout';
+import { getUser } from '@/lib/auth/session';
+import App from 'next/app';
 import { useState } from 'react';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  const [user, setUser] = useState(pageProps?.user);
+function MyApp({ Component, pageProps, user: initialUser }) {
+  const [user, setUser] = useState(initialUser);
 
   return (
     <Layout user={user} setUser={setUser}>
@@ -11,5 +13,19 @@ function MyApp({ Component, pageProps }) {
     </Layout>
   );
 }
+
+/**
+ * @param {import('next/app').AppContext} ctx
+ * @returns {Promise<import('next/app').AppInitialProps>}
+ */
+MyApp.getInitialProps = async (ctx) => {
+  const context = await App.getInitialProps(ctx);
+  try {
+    const user = await getUser(ctx.ctx.req);
+    return { ...context.pageProps, user };
+  } catch {
+    return { ...context.pageProps };
+  }
+};
 
 export default MyApp;
