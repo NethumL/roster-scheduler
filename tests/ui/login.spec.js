3ae94248685154eba60a0988ec42c262ff3d775f
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-11-16 19:39:54 +0530

    Add test for invalid credentials in login

--- tests/ui/login.spec.js
+++ tests/ui/login.spec.js
@@ -17,3 +17,18 @@ test('should redirect after successful login', async ({ page, baseURL }) => {
   expect(page.url()).toMatch(new RegExp(baseURL + '/?$'));
   await teardownDb();
 });
+
+test('should show error for invalid credentials', async ({ page, baseURL }) => {
+  await setupDb();
+  await page.goto('/auth/login');
+  expect(page.url()).toBe(baseURL + '/auth/login');
+  await page.getByLabel('Username').fill('john');
+  await page.getByLabel('Password').fill('passwor');
+  await page.getByRole('button', { name: 'Login' }).last().click();
+  await page.waitForLoadState('networkidle');
+  await expect(page.getByRole('alert').first()).toHaveText(
+    'Invalid credentials'
+  );
+  expect(page.url()).toBe(baseURL + '/auth/login');
+  await teardownDb();
+});
