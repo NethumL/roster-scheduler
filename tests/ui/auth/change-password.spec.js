145d1c34d0e7c89c13d0f3bf6f6d00f65843c019
Nethum Lamahewage <nethumpinsara@gmail.com>
2022-11-18 01:18:40 +0530

    Group tests

--- tests/ui/auth/change-password.spec.js
+++ tests/ui/auth/change-password.spec.js
@@ -4,27 +4,29 @@ import mongoose from 'mongoose';
 import test from '../fixtures';
 import { compareSync } from 'bcryptjs';
 
-test('should change password', async ({ page, baseURL }) => {
-  const credentials = { username: 'admin', password: 'password' };
-  const newPassword = 'Password1!';
+test.describe('/auth/change-password', () => {
+  test('should change password', async ({ page, baseURL }) => {
+    const credentials = { username: 'admin', password: 'password' };
+    const newPassword = 'Password1!';
 
-  await login(page, baseURL, credentials);
-  await page.goto('/auth/change-password');
-  expect(page.url()).toBe(baseURL + '/auth/change-password');
-  await page.getByLabel('Current password').fill(credentials.password);
-  await page.getByLabel('New password').fill(newPassword);
-  await page.getByLabel('Confirm password').fill(newPassword);
-  await page.getByRole('button', { name: 'Update' }).last().click();
-  await page.waitForURL(new RegExp(baseURL + '/?$'), {
-    waitUntil: 'networkidle',
+    await login(page, baseURL, credentials);
+    await page.goto('/auth/change-password');
+    expect(page.url()).toBe(baseURL + '/auth/change-password');
+    await page.getByLabel('Current password').fill(credentials.password);
+    await page.getByLabel('New password').fill(newPassword);
+    await page.getByLabel('Confirm password').fill(newPassword);
+    await page.getByRole('button', { name: 'Update' }).last().click();
+    await page.waitForURL(new RegExp(baseURL + '/?$'), {
+      waitUntil: 'networkidle',
+    });
+    expect(page.url()).toMatch(new RegExp(baseURL + '/?$'));
+    await mongoose.connect(process.env.MONGODB_URI);
+    const user = await User.findOne({ username: credentials.username })
+      .select('+password')
+      .lean();
+    expect(compareSync(newPassword, user.password)).toBe(true);
+    await login(page, baseURL, { ...credentials, password: newPassword });
   });
-  expect(page.url()).toMatch(new RegExp(baseURL + '/?$'));
-  await mongoose.connect(process.env.MONGODB_URI);
-  const user = await User.findOne({ username: credentials.username })
-    .select('+password')
-    .lean();
-  expect(compareSync(newPassword, user.password)).toBe(true);
-  await login(page, baseURL, { ...credentials, password: newPassword });
 });
 
 /**
